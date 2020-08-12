d2af9f123a34b36f3a249a2866605484c8b2174f
Akrit Shrestha <akrit.act360@gmail.com>
2020-08-12 16:59:32 +0545

    initial commit

---
+++ src/reducer.js
@@ -0,0 +1,68 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
+  playing: false,
+  item: null,
+  token:
+    "BQCS3j7wuhgd_wc-4_Oq8oF8MPQLNuJ3wKZBoyRqlGCroCyhP8FAzE85On3yQeS5Z6BbyWO8cLsCWIRrWbVPGy5qgxHeiXe5LYo6FVi5hBBgxfhYefNmEIYfUnbba4Nv7oxe6lqsO_j2qw1vw_1HF8PBoxOCOQ",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_PLAYING":
+      return {
+        ...state,
+        playing: action.playing,
+      };
+
+    case "SET_ITEM":
+      return {
+        ...state,
+        item: action.item,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+
+    case "SET_TOP_ARTISTS":
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case "SET_SPOTIFY":
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
+
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
